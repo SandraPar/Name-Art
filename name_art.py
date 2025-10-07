b46e99fc26bfef1d892e02335d05dbefc07c84c0
SandraPar <145221703+SandraPar@users.noreply.github.com>
2025-10-07 20:35:08 +0200

    Add files via upload

---
+++ name_art.py
@@ -0,0 +1,11 @@
+#Name Art Programme
+
+from art import text2art
+
+def name_art():
+    name = input("Enter your name: ")
+    art = text2art(name)
+    print(art)
+    
+# Run the proframme
+name_art()
